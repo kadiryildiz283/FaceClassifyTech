d129c82a697bae321d40bdd87355d7a583a815af
kadiryildiz283 <76630536+kadiryildiz283@users.noreply.github.com>
2023-12-28 13:25:50 +0300

    kadir
    .

--- FaceClassifyTechHUD.py
+++ FaceClassifyTechHUD.py
@@ -7,7 +7,25 @@ import os
 import urllib.request
 import tkinter.font as tkFont
 from zipfile import ZipFile
-from PIL import  ImageDraw, ImageFont
+from PIL import  ImageDraw, ImageFont, Image, ImageTk
+from UmutAtac_KadirYildiz import *
+from tkinter import filedialog
+from PIL import Image, ImageTk
+
+def open_file(file_path):
+    new_window = tk.Toplevel(root)
+    img = Image.open(file_path)
+    photo = ImageTk.PhotoImage(img)
+    label = tk.Label(new_window, image=photo)
+    label.image = photo
+    label.pack()
+
+# Tkinter arayüzü kodları
+# ...
+# Tkinter arayüzü kodları devam eder...
+kilit = []
+folder_path = ""
+kisiler = []
 def download_font(font_name, font_url):
     # Font dosyasını indir
     font_zip_path = f"{font_name}.zip"
@@ -22,16 +40,53 @@ def download_font(font_name, font_url):
     os.remove(font_zip_path)
 
     # Resim eklemek için bir boşluk oluşturmak
-def load_image():
-        image = Image.open("resim.png")  # 'resim.png' yerine kendi resim dosyanızın adını verin
+def load_image(resim):
+        image = Image.open(resim)  # 'resim.png' yerine kendi resim dosyanızın adını verin
         photo = ImageTk.PhotoImage(image)
         image_label.config(image=photo)
         image_label.image = photo  # Resmi global olarak saklamak için bu satırı ekliyoruz
+def integrate_face_recognition(folder_path):
+    # Burada yüz tanıma işlemleri yapılacak
+    # ...
+
+    # Elde edilen sonuçları tkinter arayüzünde göstermek için bir fonksiyon çağırabiliriz
+    # Tkinter penceresi oluşturma
+    root = tk.Tk()
+    root.title("Yüz Tanıma Sonuçları")
+    # Resimleri göstermek için bir frame oluşturma
+    image_frame = tk.Frame(root)
+    image_frame.pack(padx=10, pady=10)
+    # Her bir resmi ImageTk formatına dönüştürüp gösterme
+    for i, image_path in enumerate(folder_path):
+        img = Image.open(image_path)
+        img = img.resize((200, 200), Image.ANTIALIAS)  # Resmi istediğiniz boyuta küçültmek için
+        photo = ImageTk.PhotoImage(img)
+        
+        label = tk.Label(image_frame, image=photo)
+        label.image = photo  # Referansı saklamak için bu satırı ekliyoruz
+        label.grid(row=i // 4, column=i % 4, padx=10, pady=10)  # Örnek olarak 4 sütunlu bir düzen kullanıyoruz
+
+    root.mainloop()
+# Örnek bir fonksiyon, gerçek sonuçları buraya ekleyebilirsin
+
+
+def kontrol(dizi, explorer):
+    global kilit
+    kilit = biseyler(explorer, kisiler, kilit)
+
+    for i, image in enumerate(os.listdir(explorer)):
+        if dizi[i][0] == i + 1:
+            folder = "galeri/" + image
+            open_file(folder)
 
 def on_button_click():
     folder_path = filedialog.askdirectory()
+    okuma(folder_path, kisiler)
+    global kilit
+    kilit = biseyler(folder_path, kisiler, kilit)
+    kontrol(kilit,folder_path);
     if folder_path:
-        progress_window = tk.Toplevel(root)
+        progress_window = tk.Tk()
         progress_window.title("Yükleniyor...")
         progress_window.configure(bg='black')
         
@@ -66,10 +121,9 @@ root.title("Tkinter Örnek")
 
 label = tk.Label(root, text="Python arayüzüne hoş geldiniz!", font=("Hotel De Paris", 16))
 label.pack(pady=20)
-print(tk.font.families())
 
 # Buton oluşturmak
-button = tk.Button(root, text="Tıkla", command=on_button_click)
+button = tk.Button(root, text="Ara", command=on_button_click)
 button.pack()
 
 # İki seçenekli radyo butonlar oluşturmak
@@ -77,17 +131,17 @@ selected_option = tk.StringVar()
 option_frame = ttk.Frame(root)
 option_frame.pack(pady=10)
 
-option1 = ttk.Radiobutton(option_frame, text="Seçenek 1", variable=selected_option, value="option1")
+option1 = ttk.Radiobutton(option_frame, text="1 Kişilik", variable=selected_option, value="option1")
 option1.grid(row=0, column=0, padx=5)
 
-option2 = ttk.Radiobutton(option_frame, text="Seçenek 2", variable=selected_option, value="option2")
+option2 = ttk.Radiobutton(option_frame, text="2 kişilik", variable=selected_option, value="option2")
 option2.grid(row=0, column=1, padx=5)
 
 
 image_label = tk.Label(root)
 image_label.pack(pady=10)
 
-load_image()  # Resmi yüklemek için fonksiyonu çağırıyoruz
+load_image("resim.png")  # Resmi yüklemek için fonksiyonu çağırıyoruz
 
 # Ana döngüyü başlatmak
 root.mainloop()
--- UmutAtac_KadirYildiz.py
+++ UmutAtac_KadirYildiz.py
@@ -3,7 +3,6 @@ import cv2
 import os
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-
 def read_img(path):
     img = cv2.imread(path)
     (h, w) = img.shape[:2]
@@ -16,83 +15,78 @@ known_encodings = []
 known_names = []
 
 
-galeri = "galeri"
-dosyalar = os.listdir(galeri)
-resimler = []
-for dosya in dosyalar:
-    resimler.append(dosya)
-
 #print(resimler)
-
-unknown_dir = 'galeri'
-
-j=0
-sayac = 0
 kilit = []
 birkez = 1
-resimsayisi = 0
+resimsayisi = 5
 kisi = []
-
-for file in os.listdir(unknown_dir):
-    sayac += 1 
-    img_path = os.path.join(unknown_dir, file)
-    try:
-        img = read_img(img_path)
-        if img is None:
-            print(f"Error reading image: {img_path}")
-            continue  # Skip to the next image if reading fails
-
-        img_encs = face_recognition.face_encodings(img)
-        if not img_encs:
-            print(f"No face found in image: {img_path}")
-            continue  # Skip to the next image if no face is detected
-
-        face_locations = face_recognition.face_locations(img)
-        img = read_img(unknown_dir + '/' + file)
-        img_encs = face_recognition.face_encodings(img)
-        face_locations = face_recognition.face_locations(img)
-        print(file)
-        for (img_enc, (top, right, bottom, left)) in zip(img_encs, face_locations):
-            results = face_recognition.compare_faces(known_encodings, img_enc)
-            #print(face_recognition.face_distance(known_encodings, img_enc))
-            if True in results:
-                for i in range(len(results)):
-                    if results[i]:
-                        name = known_names[i]
-                        #cv2.rectangle(img, (left, top), (right, bottom), (0, 0, 255), 2)
-                        #cv2.putText(img, name, (left+2, bottom+20), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 1)
-                            #if birkez == 1:
-                                #resimsayisi = sayac - j 
-                                #print(resimsayisi)
-                                #im2 = read_img(unknown_dir + "/" + resimler[kilit[resimsayisi-1][0]-1])
-                                #cv2.imshow("img",im2)
-                                #cv2.waitKey(0)
-                                #birkez = 0
-            else:   
-                j+=1
-                #print("j = ",j)
-                #crop_img = img[top:bottom+100, left-50:right+100]
-                #cv2.imwrite('bilinen/bilinen{}.jpg'.format(j), crop_img)
-                known_encodings.append(img_enc)
-                kilit.append([sayac,"kisi",j])
-                known_names.append("kisi{}".format(j))
-                #cv2.rectangle(img, (left, top), (right, bottom), (0, 0, 255), 2)
-                #cv2.putText(img,"Bilinmeyen Kisi,{}".format(j), (left+2,bottom+20), cv2.FONT_HERSHEY_PLAIN ,1,(255,255,255),1) 
-        
-    except Exception as e:
-        print(f"An error occurred for image {img_path}: {e}")
-        continue 
+j=0
+sayac = 0
+def okuma(galeri, kilit):
+    for file in os.listdir(galeri):
+        global sayac
+        print(resimsayisi)
+        sayac +=1
+        img_path = os.path.join(galeri, file)
+        try:
+            img = read_img(img_path)
+            if img is None:
+                print(f"Error reading image: {img_path}")
+                continue  # Skip to the next image if reading fails
     
-print(kilit)
-kisiler = []
-for h , file in enumerate(os.listdir(unknown_dir)):
-    for j in range(len(kilit)):
-        if int(kilit[j][0])-1 == h:
-            kisiler.append(file)
+            img_encs = face_recognition.face_encodings(img)
+            if not img_encs:
+                print(f"No face found in image: {img_path}")
+                continue  # Skip to the next image if no face is detected
+    
+            face_locations = face_recognition.face_locations(img)
+            img = read_img(galeri + '/' + file)
+            img_encs = face_recognition.face_encodings(img)
+            face_locations = face_recognition.face_locations(img)
+            print(file)
+            for (img_enc, (top, right, bottom, left)) in zip(img_encs, face_locations):
+                results = face_recognition.compare_faces(known_encodings, img_enc)
+                #print(face_recognition.face_distance(known_encodings, img_enc))
+                if True in results:
+                    for i in range(len(results)):
+                        if results[i]:
+                            name = known_names[i]
+                            #cv2.rectangle(img, (left, top), (right, bottom), (0, 0, 255), 2)
+                            #cv2.putText(img, name, (left+2, bottom+20), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 1)
+                                #if birkez == 1:
+                                    #resimsayisi = sayac - j 
+                                    #print(resimsayisi)
+                                    #im2 = read_img(unknown_dir + "/" + resimler[kilit[resimsayisi-1][0]-1])
+                                    #cv2.imshow("img",im2)
+                                    #cv2.waitKey(0)
+                                    #birkez = 0
+                else:   
+                    global j
+                    j+=1
+                    #print("j = ",j)
+                    #crop_img = img[top:bottom+100, left-50:right+100]
+                    #cv2.imwrite('bilinen/bilinen{}.jpg'.format(j), crop_img)
+                    known_encodings.append(img_enc)
+                    kilit.append([sayac,"kisi",j])
+                    known_names.append("kisi{}".format(j))
+                    #cv2.rectangle(img, (left, top), (right, bottom), (0, 0, 255), 2)
+                    #cv2.putText(img,"Bilinmeyen Kisi,{}".format(j), (left+2,bottom+20), cv2.FONT_HERSHEY_PLAIN ,1,(255,255,255),1) 
             
-def show_images(images):
+        except Exception as e:
+            print(f"An error occurred for image {img_path}: {e}")
+            continue 
+        
+#kisiler = []
+def biseyler(galeri,dizi,kilit):
+    for h , file in enumerate(os.listdir(galeri)):
+        for j in range(len(kilit)):
+            if int(kilit[j][0])-1 == h:
+                dizi.append(file)
+    return dizi
+            
+def show_images(images,galeri):
     originalPath = os.getcwd() 
-    os.chdir(unknown_dir)
+    os.chdir(galeri)
     fig = plt.figure(figsize=(10, 10))
     columns = 4
     rows = 5
@@ -106,124 +100,129 @@ def show_images(images):
     plt.show()
     os.chdir(originalPath)
     
-show_images(kisiler)
-arama = input("tek kişi için 1'e iki kişi aramak için 2 girin")
+#show_images(kisiler)
+#arama = input("tek kişi için 1'e iki kişi aramak için 2 girin")
 d = 0
 e = 0
-if arama == "2":
- a = input("değer gir")
- b = input("değer gir")
- for file in os.listdir(unknown_dir):
-    sayac += 1 
-    img_path = os.path.join(unknown_dir, file)
-    try:
-        img = read_img(img_path)
-        if img is None:
-            print(f"Error reading image: {img_path}")
-            continue  # Skip to the next image if reading fails
-
-        img_encs = face_recognition.face_encodings(img)
-        if not img_encs:
-            print(f"No face found in image: {img_path}")
-            continue  # Skip to the next image if no face is detected
-
-        face_locations = face_recognition.face_locations(img)
-        print("resim okundu", file)
-        print("---------------------")
-        for (img_enc, (top, right, bottom, left)) in zip(img_encs, face_locations):
-            results = face_recognition.compare_faces(known_encodings, img_enc)
-            #print(face_recognition.face_distance(known_encodings, img_enc))
-            if True in results:
-                for i in range(len(results)):
-                    if results[i]:
-                        name = known_names[i]
-                        cv2.rectangle(img, (left, top), (right, bottom), (0, 0, 255), 2)
-                        cv2.putText(img, name, (left+2, bottom+20), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 1)
-                        #cv2.imshow("img",img)
-                        #cv2.waitKey(0)
-                        if e == 1:
-                            if a == name:
-                                e=0
-                                d=0
-                                print("çalıştı d")
-                                kisi.append(file)
-                        if d == 1:
+def ikikisiarama(galeri,arama):
+    if arama == "2":
+     a = input("değer gir")
+     b = input("değer gir")
+     for file in os.listdir(galeri):
+        global sayac
+        sayac += 1 
+        img_path = os.path.join(galeri, file)
+        try:
+            img = read_img(img_path)
+            if img is None:
+                print(f"Error reading image: {img_path}")
+                continue  # Skip to the next image if reading fails
+    
+            img_encs = face_recognition.face_encodings(img)
+            if not img_encs:
+                print(f"No face found in image: {img_path}")
+                continue  # Skip to the next image if no face is detected
+    
+            face_locations = face_recognition.face_locations(img)
+            print("resim okundu", file)
+            print("---------------------")
+            for (img_enc, (top, right, bottom, left)) in zip(img_encs, face_locations):
+                results = face_recognition.compare_faces(known_encodings, img_enc)
+                #print(face_recognition.face_distance(known_encodings, img_enc))
+                if True in results:
+                    for i in range(len(results)):
+                        if results[i]:
+                            name = known_names[i]
+                            cv2.rectangle(img, (left, top), (right, bottom), (0, 0, 255), 2)
+                            cv2.putText(img, name, (left+2, bottom+20), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 1)
+                            #cv2.imshow("img",img)
+                            #cv2.waitKey(0)
+                            global e
+                            if  e == 1:
+                                if a == name:
+                                    e=0
+                                    d=0
+                                    print("çalıştı d")
+                                    kisi.append(file)
+                            if d == 1:
+                                if b == name:
+                                    d=0
+                                    e=0
+                                    print("çalıştı e")
+                                    kisi.append(file)
+                            if a == name: 
+                                    d = 1
+                                    e = 0
                             if b == name:
-                                d=0
-                                e=0
-                                print("çalıştı e")
-                                kisi.append(file)
-                        if a == name: 
-                                d = 1
-                                e = 0
-                        if b == name:
-                                e= 1 
-                                d= 0
-                        print("d   " ,d)
-                        print("e   " , e)
-                            #if birkez == 1:
-                                #resimsayisi = sayac - j 
-                                #print(resimsayisi)
-                                #im2 = read_img(unknown_dir + "/" + resimler[kilit[resimsayisi-1][0]-1])
-                                #cv2.imshow("img",im2)
-                                #cv2.waitKey(0)
-                                #birkez = 0
-        
-    except Exception as e:
-        print(f"An error occurred for image {img_path}: {e}")
-        continue  # Skip to the next image if any exception occurs
+                                    e= 1 
+                                    d= 0
+                            print("d   " ,d)
+                            print("e   " , e)
+                                #if birkez == 1:
+                                    #resimsayisi = sayac - j 
+                                    #print(resimsayisi)
+                                    #im2 = read_img(unknown_dir + "/" + resimler[kilit[resimsayisi-1][0]-1])
+                                    #cv2.imshow("img",im2)
+                                    #cv2.waitKey(0)
+                                    #birkez = 0
+            
+        except Exception as e:
+            print(f"An error occurred for image {img_path}: {e}")
+            continue  # Skip to the next image if any exception occurs
 
     
 
-    d = 0
-    e = 0
+        d = 0
+        e = 0
 
-show_images(kisi)
-if arama == "1":
-        print("girdi mi")
-        a = input("değer gir")
-        for file in os.listdir(unknown_dir):
-         sayac += 1 
-         img_path = os.path.join(unknown_dir, file)
-         try:
-           img = read_img(img_path)
-           if img is None:
-             print(f"Error reading image: {img_path}")
-             continue  # Skip to the next image if reading fails
+#show_images(kisi)
+def tekkisiarama(galeri):
+    if arama == "1":
+            print("girdi mi")
+            a = input("değer gir")
+            for file in os.listdir(galeri):
+             global sayac
+             sayac += 1 
+             img_path = os.path.join(galeri, file)
+             try:
+               img = read_img(img_path)
+               if img is None:
+                 print(f"Error reading image: {img_path}")
+                 continue  # Skip to the next image if reading fails
+    
+               img_encs = face_recognition.face_encodings(img)
+               if not img_encs:
+                print(f"No face found in image: {img_path}")
+                continue  # Skip to the next image if no face is detected
+    
+               face_locations = face_recognition.face_locations(img)
+               print("resim okundu", file)
+               print("---------------------")
+               for (img_enc, (top, right, bottom, left)) in zip(img_encs, face_locations):
+                               if not face_locations:
+                                   continue  
+                               results = face_recognition.compare_faces(known_encodings, img_enc)
+                               #print(face_recognition.face_distance(known_encodings, img_enc))
+                               if True in results:
+                                   for i in range(len(results)):
+                                       if results[i]:
+                                           name = known_names[i]
+                                           cv2.rectangle(img, (left, top), (right, bottom), (0, 0, 255), 2)
+                                           cv2.putText(img, name, (left+2, bottom+20), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 1)
+                                           #cv2.imshow("img",img)
+                                           #cv2.waitKey(0)
+                                           if a == name:
+                                            kisi.append(file)
+                                               #if birkez == 1:
+                                                   #resimsayisi = sayac - j 
+                                                   #print(resimsayisi)
+                                                   #im2 = read_img(unknown_dir + "/" + resimler[kilit[resimsayisi-1][0]-1])
+                                                   #cv2.imshow("img",im2)
+                                                   #cv2.waitKey(0)
+                                                   #birkez = 0
+            
+             except Exception as e:
+               print(f"An error occurred for image {img_path}: {e}")
+             continue  # Skip to the next image if any exception occurs
 
-           img_encs = face_recognition.face_encodings(img)
-           if not img_encs:
-            print(f"No face found in image: {img_path}")
-            continue  # Skip to the next image if no face is detected
-
-           face_locations = face_recognition.face_locations(img)
-           print("resim okundu", file)
-           print("---------------------")
-           for (img_enc, (top, right, bottom, left)) in zip(img_encs, face_locations):
-                           if not face_locations:
-                               continue  
-                           results = face_recognition.compare_faces(known_encodings, img_enc)
-                           #print(face_recognition.face_distance(known_encodings, img_enc))
-                           if True in results:
-                               for i in range(len(results)):
-                                   if results[i]:
-                                       name = known_names[i]
-                                       cv2.rectangle(img, (left, top), (right, bottom), (0, 0, 255), 2)
-                                       cv2.putText(img, name, (left+2, bottom+20), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 1)
-                                       #cv2.imshow("img",img)
-                                       #cv2.waitKey(0)
-                                       if a == name:
-                                        kisi.append(file)
-                                           #if birkez == 1:
-                                               #resimsayisi = sayac - j 
-                                               #print(resimsayisi)
-                                               #im2 = read_img(unknown_dir + "/" + resimler[kilit[resimsayisi-1][0]-1])
-                                               #cv2.imshow("img",im2)
-                                               #cv2.waitKey(0)
-                                               #birkez = 0
-        
-         except Exception as e:
-           print(f"An error occurred for image {img_path}: {e}")
-         continue  # Skip to the next image if any exception occurs
-
-show_images(kisi)
+#show_images(kisi)
